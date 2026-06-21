1b322d67f5682be168baca26816fb64f7c5958bd
Taylor Ren <taylor.ren@gmail.com>
2026-06-21 09:55:03 +0800

    1164 updated with 1-base and clearer

--- 11.regression/p01164.cpp
+++ 11.regression/p01164.cpp
@@ -1,33 +1,62 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+const int MAX_N = 100;
+const int MAX_M = 1000;
+
+int countWays1D(const int prices[], int N, int M)
+{
+    int dp[MAX_M + 1] = {0};
+    dp[0] = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        int price = prices[i];
+        for (int j = M; j >= price; j--)
+        {
+            dp[j] += dp[j - price];
+        }
+    }
+
+    return dp[M];
+}
+
+int countWays2D(const int prices[], int N, int M)
+{
+    // dp[i][sum]: number of ways to make exact amount 'sum' using the first i items.
+    int dp[MAX_N + 1][MAX_M + 1] = {0};
+    dp[0][0] = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        int price = prices[i];
+        for (int sum = 0; sum <= M; sum++)
+        {
+            // Case 1: do not take item i.
+            dp[i][sum] = dp[i - 1][sum];
+            if (sum >= price)
+            {
+                // Case 2: take item i, so previous sum is (sum - price).
+                dp[i][sum] += dp[i - 1][sum - price];
+            }
+        }
+    }
+
+    return dp[N][M];
+}
 
 int main()
 {
     int N, M;
     cin >> N >> M;
 
-    vector<int> prices(N);
-    for (int i = 0; i < N; i++)
+    int prices[MAX_N + 1] = {0};
+    for (int i = 1; i <= N; i++)
     {
         cin >> prices[i];
     }
 
-    // dp[i] 表示总价格恰好为i的方案数
-    vector<int> dp(M + 1, 0);
-    dp[0] = 1;  // 不选任何菜，价格为0的方案数为1
-
-    // 对每种菜品进行动态规划
-    for (int i = 0; i < N; i++)
-    {
-        // 从大到小遍历，避免重复计算
-        for (int j = M; j >= prices[i]; j--)
-        {
-            dp[j] += dp[j - prices[i]];
-        }
-    }
-
-    cout << dp[M] << endl;
+    cout << countWays2D(prices, N, M) << endl;
 
     return 0;
 }
